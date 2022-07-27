192483a90fce07d40dda99bd4d26bdc709e38a27
addy360 <abbyboy360@gmail.com>
2022-07-27 10:11:46 +0300

    initial commit

---
+++ src/main/java/com/addy360/openApiSpec/OpenApiSpecApplication.java
@@ -0,0 +1,13 @@
+package com.addy360.openApiSpec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiSpecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addy360/openApiSpec/OpenApiSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addy360.openApiSpec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
